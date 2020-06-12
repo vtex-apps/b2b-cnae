81d66146824ac3e46d19199e8c24b1633af4eaa2
Everton Ataide <everton.ataide@vtex.com.br>
2020-06-12 09:35:31 -0300

    Documentation v1

--- node/utils/token.ts
+++ node/utils/token.ts
@@ -1,19 +1,21 @@
-export async function GetOrGenerateToken(login: string, password: string, ctx: Context) {
-    const {
-        clients: { serpro },
-    } = ctx
+export async function GetOrGenerateToken(
+  login: string,
+  password: string,
+  ctx: Context
+) {
+  const {
+    clients: { serpro },
+  } = ctx
 
-    let response: any = null
+  let response: any = null
 
-    try {
-        response = await serpro.getToken(login, password)
-        if (response.access_token) {
-            return response.access_token
-        }
-        else
-            return ''
-    } catch (error) {
-        console.log('Error generating new token', error)
-        return '';
+  try {
+    response = await serpro.getToken(login, password)
+    if (response.access_token) {
+      return response.access_token
     }
+    return ''
+  } catch (error) {
+    return ''
+  }
 }
